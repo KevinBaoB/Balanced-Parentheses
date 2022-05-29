d2b38bcc403b09f583a8edee3bea541abbf15965
KevinBaoB <kevinbaobui@gmail.com>
2022-05-28 21:29:23 -0500

    Pain

--- js/balanceParens.js
+++ js/balanceParens.js
@@ -1,6 +1,26 @@
-balanceParens = (str) =>
-{
+balanceParens = (str) => {
+  const res = [];
+  const splitStr = str.split("");
+  for (let i = 0; i < str.length; i++) {
+    const item = str[i];
+    if (item === "(") res.push(i);
+    else if (item === ")") {
+      if (res.length === 0) {
+        splitStr[i] = "";
+      } else {
+        res.pop();
+      }
+    }
+  }
+  for (let i = 0; i < res.length; i++) {
+    const item = res[i];
+    splitStr[item] = "";
+  }
 
-}
+  return splitStr.join("");
+};
 
-module.exports = { balanceParens }
+console.log(balanceParens("a(b)c)"));
+console.log(balanceParens("()"));
+console.log(balanceParens(")("));
+// module.exports = { balanceParens };
