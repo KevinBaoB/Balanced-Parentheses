1d05c3be9e1617f4e588c1ecd0567af1e33892ae
KevinBaoB <kevinbaobui@gmail.com>
2022-05-29 17:11:31 -0500

    Translated

--- python/balance_parens.py
+++ python/balance_parens.py
@@ -1,2 +1,19 @@
-def balance_parens(str):
-    pass
+def balance_parens(par) :
+    stack=[]
+    split_str=list(par)
+    for i in range(len(par)):
+        if par[i]=='(':
+            stack.append(i)
+        elif par[i]==')':
+            if len(stack) !=0:
+                    stack.pop()
+            else:
+                split_str[i]=""
+    for i in stack:
+        split_str[i]=""
+    return '' .join(split_str)
+
+
+print(balance_parens("a(b)c)"))
+print(balance_parens('()'))
+print(balance_parens(')('))
